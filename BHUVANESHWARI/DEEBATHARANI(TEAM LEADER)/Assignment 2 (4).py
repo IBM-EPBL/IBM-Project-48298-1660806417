27249fc6faae11faf5ba4560682b8464344eab9d
Deebatharani-07 <113497271+Deebatharani-07@users.noreply.github.com>
2022-11-19 23:51:21 +0530

    Delete BHUVANESHWARI/DEEBATHARANI(TEAM LEADER) directory

--- BHUVANESHWARI/DEEBATHARANI(TEAM LEADER)/Assignment 2 (4).py
+++
@@ -1,6 +0,0 @@
-CREATE TABLE [dbo].[Users] (
-   [UserId] [int] NOT NULL,
-   [UserName] [varchar] (15) NOT NULL ,
-   [Pwd] [varchar] (25) NOT NULL ,
-   [userRole] [varchar] (25) NOT NULL ,
-) ON [PRIMARY]
